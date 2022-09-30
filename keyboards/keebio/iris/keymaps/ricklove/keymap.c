acfd91b985f7d0b7b7ec81ee3475b4f20aa62fd4
Rick Love <rick@toldpro.com>
2022-09-29 23:41:14 -0500

    set layers

--- keyboards/keebio/iris/keymaps/ricklove/keymap.c
+++ keyboards/keebio/iris/keymaps/ricklove/keymap.c
@@ -31,43 +31,43 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_LOWER] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,                           KC_TRANS,KC_TRANS,KC_PGUP, KC_TRANS,KC_TRANS,KC_TRANS,
+     _______, _______, _______, _______, _______, _______,                            _______, _______, KC_PGUP, _______, _______, _______, 
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,                           KC_TRANS,KC_HOME, KC_UP,   KC_END,  KC_TRANS,KC_TRANS,
+     _______, _______, _______, _______, _______, _______,                            _______, KC_HOME, KC_UP,   KC_END,  _______, _______, 
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,                           KC_TRANS,KC_LEFT, KC_DOWN, KC_RGHT, KC_TRANS,KC_TRANS,
+     _______, _______, _______, _______, _______, _______,                            _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, 
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,         KC_TRANS,KC_TRANS,KC_TRANS,KC_PGDN, KC_TRANS,KC_TRANS,KC_TRANS,
+     _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, KC_PGDN, _______, _______, _______, 
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    KC_TRANS,KC_TRANS,KC_TRANS,                  KC_TRANS,KC_TRANS,KC_TRANS
+                                    _______, _______, _______,                   _______, _______, _______
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
   [_RAISE] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,                           KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,
+     _______, _______, KC_F4,   KC_F8,   KC_F12,  _______,                            _______, KC_7,    KC_8,    KC_9,    _______, _______, 
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,                           KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,
+     _______, _______, KC_F3,   KC_F7,   KC_F11,  _______,                            _______, KC_4,    KC_5,    KC_6,    _______, _______, 
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,                           KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,
+     _______, _______, KC_F2,   KC_F6,   KC_F10,  _______,                            _______, KC_1,    KC_2,    KC_3,    _______, _______, 
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,         KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,
+     _______, _______, KC_F1,   KC_F5,   KC_F9,   _______, _______,          _______, _______, KC_0,    _______, _______, _______, _______, 
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    KC_TRANS,KC_TRANS,KC_TRANS,                  KC_TRANS,KC_TRANS,KC_TRANS
+                                    _______, _______, _______,                   _______, _______, _______
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
   [_ADJUST] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,                           KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,
+     _______, _______, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                            KC_CIRC, KC_LBRC, KC_RBRC, _______, _______, _______, 
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,                           KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,
+     _______, _______, KC_DQUO, KC_QUOT, KC_GRV,  KC_QUES,                            KC_PMNS, KC_LCBR, KC_RCBR, _______, _______, _______, 
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,                           KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,
+     _______, KC_COLN, KC_SCLN, KC_COMM, KC_DOT,  KC_EQL,                             KC_PPLS, KC_LPRN, KC_RPRN, KC_SLSH, KC_BSLS, _______, 
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,         KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,KC_TRANS,
+     _______, _______, KC_TILD, KC_AMPR, KC_PIPE, KC_EXLM, _______,          _______, KC_PAST, KC_LT,   KC_GT,   _______, _______, _______, 
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    KC_TRANS,KC_TRANS,KC_TRANS,                  KC_TRANS,KC_TRANS,KC_TRANS
+                                    _______, _______, _______,                   _______, _______, _______
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   )
 };
